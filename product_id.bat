96766ede8c68d516cdcd15ef3379e6ab7dea405b
Vouu <contact@mtz.pw>
2023-03-08 10:07:30 +0700

    update vls

--- product_id.bat
+++ product_id.bat
@@ -10,9 +10,9 @@ for /f "usebackq delims=" %%i in ("%file%") do (
 )
 
 set /a index=%random% %% count + 1
-set "randomLine=!line[%index%]!"
+set "product_id=!line[%index%]!"
 
 :: line da la mot so trong list cua file product_id.txt muon them nhieu thi them vao file do
 echo Modifying registry keys...
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v "ProductId" /d "%randomLine%" -f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v "ProductId" /d "%product_id%" -f
 echo Finish updating registry...
