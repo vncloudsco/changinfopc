0c2a0a816777042f267b506aece016831b2a2ba8
Vouu <contact@mtz.pw>
2023-03-08 10:04:35 +0700

    Update Product ID

---
+++ product_id.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+set file=product_id.txt
+
+set /a count=0
+for /f "usebackq delims=" %%i in ("%file%") do (
+  set /a count+=1
+  set "line[!count!]=%%i"
+)
+
+set /a index=%random% %% count + 1
+set "randomLine=!line[%index%]!"
+
+:: line da la mot so trong list cua file product_id.txt muon them nhieu thi them vao file do
+echo Modifying registry keys...
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v "ProductId" /d "%randomLine%" -f
+echo Finish updating registry...
